c13c8327524f1ab9bc3be96303ef00a46c251f3e
Andy Lindeman <alindeman@gmail.com>
2011-05-12 07:47:28 -0400

    Add seen module

--- bot.js
+++ bot.js
@@ -6,7 +6,11 @@ require('./config.js')
 redis_client = redis.createClient(redis_options.port, redis_options.host)
 irc_client = new irc.Client(options.server, options.nickName, options)
 
+// Libraries/support
+pretty_date = require('./lib/pretty.js')
+
 // TODO: Autoload everything in modules/ directory?
 require('./modules/notes.js')
 require('./modules/git.js')
 require('./modules/links.js')
+require('./modules/seen.js')
---
+++ modules/seen.js
@@ -0,0 +1,21 @@
+// logs time a user says anything
+irc_client.addListener('message', function(from, to, text) {
+  if (to.match(/^#/)) { // sent to a channel
+    seen_object = [new Date().valueOf(), text]
+    redis_client.set('seen:' + from.toLowerCase(), JSON.stringify(seen_object))
+  }
+})
+
+// ~seen <nick>
+irc_client.addListener('message', function(from, to, text) {
+  if (matches = text.match(/^~seen (\S+)/)) {
+    redis_client.get('seen:' + matches[1].toLowerCase(), function(err, obj) {
+      if (obj == null) {
+        irc_client.say(to, 'I have never seen ' + matches[1])
+      } else {
+        seen_object = JSON.parse(obj)
+        irc_client.say(to, 'I saw ' + matches[1] + ' ' + pretty_date.prettyDate(new Date(seen_object[0])) + ' saying "' + seen_object[1] + '"')
+      }
+    })
+  }
+})
