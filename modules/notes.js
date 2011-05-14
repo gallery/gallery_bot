c3c2aae30b67a41899fdbb1b9d503e61ced8ae8b
Andy Lindeman <alindeman@gmail.com>
2011-05-14 07:51:11 -0400

    Fix bug in notes modules where only one note would be listed at a time

--- modules/notes.js
+++ modules/notes.js
@@ -7,17 +7,27 @@ irc_client.addListener('message', function(from, to, text) {
   }
 })
 
+function list_notes(nick, num_notes) {
+  if (!num_notes) {
+    num_notes = 0
+  }
+
+  redis_client.rpop('notes:' + nick.toLowerCase(), function(err, obj) {
+    if (obj == null && num_notes == 0) {
+      irc_client.say(nick, '[No Notes]')
+    } else if (obj != null) {
+      note_object = JSON.parse(obj)
+      irc_client.say(nick, '[From: ' + note_object[0] + '] ' + note_object[1])
+
+      // Recurse to list all notes
+      list_notes(nick, ++num_notes)
+    }
+  })
+}
+
 // ~list-notes
 irc_client.addListener('message', function(from, to, text) {
   if (matches = text.match(/^~list-notes/i)) {
-    var num_notes = 0
-    redis_client.rpop('notes:' + from.toLowerCase(), function(err, obj) {
-      if (obj == null && num_notes == 0) {
-        irc_client.say(from, '[No Notes]')
-      } else if (obj != null) {
-        note_object = JSON.parse(obj)
-        irc_client.say(from, '[From: ' + note_object[0] + '] ' + note_object[1])
-      }
-    })
+    list_notes(from)
   }
 })
