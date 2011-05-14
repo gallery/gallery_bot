6d8aaa4ccb704b9bb281f784b40e9c39e5219f5e
Andy Lindeman <alindeman@gmail.com>
2011-05-14 07:55:40 -0400

    [Closes #10] Add ~code command

--- modules/links.js
+++ modules/links.js
@@ -4,3 +4,10 @@ irc_client.addListener('message', function(from, to, text) {
     irc_client.say(to, 'Paste stuff here (rather than in the channel): http://tools.gallery2.org/pastebin/?sesame')
   }
 })
+
+// ~code
+irc_client.addListener('message', function(from, to, text) {
+  if (matches = text.match(/^~code (\S+)/i)) {
+    irc_client.say(to, 'https://github.com/gallery/gallery3/blob/master/' + matches[1])
+  }
+})
