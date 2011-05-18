13497bda80d19ed21dbf0404f7c7e2758071f333
Andy Lindeman <alindeman@gmail.com>
2011-05-18 12:08:53 -0400

    [Finishes #1] Add notification of available notes when user joins channel

--- modules/notes.js
+++ modules/notes.js
@@ -1,12 +1,3 @@
-// note to <nick>: ...
-irc_client.addListener('message', function(from, to, text) {
-  if (matches = text.match(/^note to (\S+): (.*)$/i)) {
-    redis_client.rpush('notes:' + matches[1].toLowerCase(), JSON.stringify([from, matches[2]]), function(err, res) {
-      irc_client.send('NOTICE', from, 'Note Recorded!')
-    })
-  }
-})
-
 function list_notes(nick, num_notes) {
   if (!num_notes) {
     num_notes = 0
@@ -25,9 +16,35 @@ function list_notes(nick, num_notes) {
   })
 }
 
+// callback is function(num_notes)
+function num_notes_available(nick, callback) {
+  redis_client.llen('notes:' + nick.toLowerCase(), function(err, obj) { callback(obj) })
+}
+
+// note to <nick>: ...
+irc_client.addListener('message', function(from, to, text) {
+  if (matches = text.match(/^note to (\S+): (.*)$/i)) {
+    redis_client.rpush('notes:' + matches[1].toLowerCase(), JSON.stringify([from, matches[2]]), function(err, res) {
+      irc_client.send('NOTICE', from, 'Note Recorded!')
+    })
+  }
+})
+
+
 // ~list-notes
 irc_client.addListener('message', function(from, to, text) {
   if (matches = text.match(/^~list-notes/i)) {
     list_notes(from)
   }
+})
+
+
+
+// notification of available notes when user joins channel
+irc_client.addListener('join', function(channel, nick) {
+  num_notes_available(nick, function(num_notes) {
+    if (num_notes > 0) {
+      irc_client.send('NOTICE', nick, 'Notes are available for you. Send ~list-notes in the channel or via /msg to view them')
+    }
+  })
 })
